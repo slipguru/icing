02eb80990e41e37e75687c0bbc615bfe416260a6
Federico T <fdtomasi@gmail.com>
2016-06-09 10:25:25 +0200

    const removed from d_matrix_omp

--- ignet/parallel/d_matrix_omp.pyx
+++ ignet/parallel/d_matrix_omp.pyx
@@ -94,18 +94,18 @@ cdef double* dist_matrix(list3, list4):
     if not M:
         raise MemoryError()
     cdef char * elem_1
-    cdef const char ** list1 = <char**> malloc(n * sizeof(char*));
+    cdef char ** list1 = <char**> malloc(n * sizeof(char*));
     if not list1:
         raise MemoryError()
-    for i in range(n):
-        list1[i] = <const char *>PyString_AsString(list3[i])
+    for i in xrange(n):
+        list1[i] = PyString_AsString(list3[i])
         fprintf(stderr, "list1 %s\n", list1[i])
 
-    cdef const char ** list2 = <char**> malloc(m * sizeof(char*));
+    cdef char ** list2 = <char**> malloc(m * sizeof(char*));
     if not list2:
         raise MemoryError()
-    for i in range(m):
-        list2[i] = <const char *>PyString_AsString(list4[i])
+    for i in xrange(m):
+        list2[i] = PyString_AsString(list4[i])
         fprintf(stderr, "list2 %s\n", list2[i])
 
     # for i in range(n*m):
