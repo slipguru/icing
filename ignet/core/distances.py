6d313267e43c2f2cd873b2cfc4f94d74ffe8a5a8
Federico Tomasi <fdtomasi@gmail.com>
2016-06-22 17:49:03 +0200

    add pam, blosum models. model updated

--- ignet/core/distances.py
+++ ignet/core/distances.py
@@ -39,6 +39,69 @@ def get_nmers(sequences, n):
     return nmers
 
 
+def single_distance(seq1, seq2, n, dist_mat, norm, sym, mutations, tol=3,
+                     c=35., length_constraint=True):
+    """[DEPRECATED] Calculate a distance between two input sequences.
+
+    :param seq1: first sequence
+    :param seq2: second sequence
+    :param n: length of n-mers to be used in calculating distance
+    :param dist_mat: pandas DataFrame of mutation distances
+    :param norm: normalization method
+    :param sym: symmetry method
+    :return: numpy matrix of pairwise distances between input sequences
+    """
+    import re
+    if length_constraint and 0 < abs(len(seq1)-len(seq2)) <= tol:
+        # different lengths, seqs alignment
+        seq1, seq2 = map((lambda x: re.sub('[\.-]', 'N', str(x))),
+                         align.globalxx(seq1, seq2)[0][:2])
+
+    nmers = get_nmers([seq1, seq2], n)
+    # Iterate over combinations of input sequences
+    mutated = [i for i, (c1, c2) in enumerate(izip(seq1, seq2)) if c1 != c2]
+    seqq1, seqq2 = ['']*len(mutated), ['']*len(mutated)
+    nmer1, nmer2 = ['']*len(mutated), ['']*len(mutated)
+    for i, m in enumerate(mutated):
+        seqq1[i] = seq1[m]
+        seqq2[i] = seq2[m]
+        nmer1[i] = nmers[seq1][m]
+        nmer2[i] = nmers[seq2][m]
+
+    # Determine normalizing factor
+    if norm == 'len':
+        norm_by = len(seq1)
+    elif norm == 'mut':
+        norm_by = len(mutated)
+    elif norm == 'max':
+        norm_by = max(len(seq1), len(seq2))
+    elif norm == 'min':
+        norm_by = min(len(seq1), len(seq2))
+    else:
+        norm_by = 1
+
+    # Determine symmetry function
+    if sym == 'avg':
+        sym_fun = np.mean
+    elif sym == 'min':
+        sym_fun = min
+    else:
+        sym_fun = sum
+
+    if length_constraint and abs(len(seq1)-len(seq2)) > tol:
+        return min(len(seq1), len(seq2)) / norm_by
+    else:
+        _dist = sum([sym_fun([float(dist_mat.at[c1,n2]),float(dist_mat.at[c2,n1])]) \
+                 for c1,c2,n1,n2 in izip(seqq1,seqq2,nmer1,nmer2)]) / (norm_by)
+        if mutations:
+            try:
+                alpha_mut = np.poly1d(np.load("polyfit_arguments_2.npy"))
+                _dist *= (np.max(alpha_mut(np.max([mutations])), 0) + 0.2)
+            except:
+                _dist *= np.exp(-np.max(mutations) / c)
+        return _dist
+
+
 def junction_distance(seq1, seq2, n, dist_mat, norm, sym, tol=3, c=35.,
                       length_constraint=True):
     """[DEPRECATED] Calculate a distance between two input sequences.
@@ -115,7 +178,7 @@ def junction_distance(seq1, seq2, n, dist_mat, norm, sym, tol=3, c=35.,
                 for c1, c2, n1, n2 in izip(seqq1, seqq2, nmer1, nmer2)]) / (norm_by)
 
 
-def string_distance(seq1, seq2, dist_mat, norm_by, tol=3,
+def string_distance(seq1, seq2, dist_mat, norm_by=1, tol=3,
                     length_constraint=True):
     """Calculate a distance between two input sequences.
 
@@ -148,6 +211,7 @@ def string_distance(seq1, seq2, dist_mat, norm_by, tol=3,
         if 0 < abs(l1 - l2) <= tol:
             # different lengths, seqs alignment
             seq1, seq2 = map(extra.junction_re, align.globalxx(seq1, seq2)[0][:2])
-            norm_by = len(seq1)  # lengths could be changed
 
-    return sum([np.mean((float(dist_mat.at[c1, c2]), float(dist_mat.at[c2, c1]))) for c1, c2 in izip(list(seq1), list(seq2)) if c1 != c2]) / norm_by
+    norm_by = len(seq1) * np.max(dist_mat.as_matrix())
+    # return sum([np.mean((float(dist_mat.at[c1, c2]), float(dist_mat.at[c2, c1]))) for c1, c2 in izip(list(seq1), list(seq2)) if c1 != c2]) / norm_by
+    return sum([np.mean((float(dist_mat.at[c1, c2]), float(dist_mat.at[c2, c1]))) for c1, c2 in izip(list(seq1), list(seq2))]) / norm_by
---
+++ ignet/models/model.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+"""Distance models available for ignet."""
+import os
+import sys
+import numpy as np
+import pandas as pd
+
+from itertools import product
+
+from ..externals.DbCore import scoreAA, scoreDNA
+
+
+def char_dist_matrix(mat=None, n_score=0, gap_score=0, alphabet='dna'):
+    """Generate a distance matrix between characters.
+
+    Parameters
+    ----------
+
+    mat = input distance matrix to extend to full alphabet;
+          if unspecified, creates Hamming distance matrix that incorporates
+          IUPAC equivalencies
+    n_score = score for all matches against an N character
+    gap_score = score for all matches against a [-, .] character
+    alphabet = the type of score dictionary to generate;
+               one of [dna, aa] for DNA and amino acid characters
+
+    Returns:
+    a distance matrix (pandas DataFrame)
+    """
+    if alphabet == 'dna':
+        IUPAC_chars = list('-.ACGTRYSWKMBDHVN')
+        n = 'N'
+        score_func = scoreDNA
+    elif alphabet == 'aa':
+        IUPAC_chars = list('-.*ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        n = 'X'
+        score_func = scoreAA
+    else:
+        sys.stderr.write('ERROR: The alphabet %s unrecognised.\n'.format(alphabet))
+
+    # Default matrix to inf
+    dist_mat = pd.DataFrame(float('inf'), index=IUPAC_chars,
+                            columns=IUPAC_chars, dtype=float)
+    # Set gap score
+    for c in '-.':
+        dist_mat.loc[c] = dist_mat.loc[:, c] = gap_score
+    # Set n score
+    dist_mat.loc[n] = dist_mat.loc[:, n] = n_score
+    # Fill in provided distances from input matrix
+    if mat is not None:
+        for i, j in product(mat.index, mat.columns):
+            dist_mat.loc[i, j] = mat.loc[i, j]
+    # If no input matrix, create IUPAC-defined Hamming distance
+    else:
+        for i, j in product(dist_mat.index, dist_mat.columns):
+            dist_mat.loc[i, j] = 1 - score_func(i, j, n_score=1-n_score,
+                                                gap_score=1-gap_score)
+
+    # print dist_mat # added by toma
+    return dist_mat
+
+
+def model_matrix(model, n_score=0, gap_score=0):
+    """Get char dist matrix from model name.
+
+    Parameters
+    ----------
+    model : ('ham', 'aa', 'hs1f', 'smith96' or 'm1n', 'hs5f')
+        Model for character differences.
+    n_score : float
+        Score to assign to 'N' characters.
+    gap_score : float
+        Score to assign to GAP characters.
+
+    Returns
+    -------
+    dist_mat : pandas.DataFrame
+        Distance matrix between characters.
+    """
+    if model == 'aa':
+        # Amino acid Hamming distance
+        # n_score is overrided in case of aa
+        aa_model = char_dist_matrix(n_score=1, gap_score=gap_score, alphabet='aa')
+        return aa_model
+    elif model == 'blosum50':
+        model_path = os.path.dirname(os.path.realpath(__file__))
+        blosum50_file = os.path.join(model_path, 'blosum50.csv')
+        blosum50 = pd.read_csv(blosum50_file, header=0, index_col=0)  # in [-5,15]
+        blosum50 += abs(np.min(blosum50.as_matrix()))  # now in [0,20]
+        # it is a similarity score. Convert it to distance score.
+        return np.max(blosum50.as_matrix()) - blosum50
+    elif model == 'pam30':
+        model_path = os.path.dirname(os.path.realpath(__file__))
+        pam30_file = os.path.join(model_path, 'pam30.csv')
+        pam30 = pd.read_csv(pam30_file, header=0, index_col=0)
+        pam30 += abs(np.min(pam30.as_matrix()))
+        # it is a similarity score. Convert it to distance score.
+        return np.max(pam30.as_matrix()) - pam30
+    elif model == 'ham':
+        # DNA Hamming distance
+        ham_model = char_dist_matrix(n_score=n_score, gap_score=gap_score, alphabet='dna')
+        return ham_model
+    elif model in ('m1n', 'smith96'):
+        # Mouse 1-mer model
+        smith96 = pd.DataFrame([[0.00, 2.86, 1.00, 2.14],
+                                [2.86, 0.00, 2.14, 1.00],
+                                [1.00, 2.14, 0.00, 2.86],
+                                [2.14, 1.00, 2.86, 0.00]],
+                               index=['A', 'C', 'G', 'T'],
+                               columns=['A', 'C', 'G', 'T'], dtype=float)
+        m1n_model = char_dist_matrix(smith96, n_score=n_score, gap_score=gap_score)
+        return m1n_model
+    elif model == 'hs1f':
+        # Human 1-mer model
+        hs1f = pd.DataFrame([[0.00, 2.08, 1.00, 1.75],
+                             [2.08, 0.00, 1.75, 1.00],
+                             [1.00, 1.75, 0.00, 2.08],
+                             [1.75, 1.00, 2.08, 0.00]],
+                            index=['A', 'C', 'G', 'T'],
+                            columns=['A', 'C', 'G', 'T'], dtype=float)
+        hs1f_model = char_dist_matrix(hs1f, n_score=n_score, gap_score=gap_score)
+        return hs1f_model
+    elif model == 'hs5f':
+        # Human 5-mer DNA model
+        # model_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'models')
+        model_path = os.path.dirname(os.path.realpath(__file__))
+        hs5f_file = os.path.join(model_path, 'HS5F_Distance.tab')
+        hs5f_model = pd.read_csv(hs5f_file, sep='\t', index_col=0)
+        return hs5f_model
+    else:
+        sys.stderr.write('Unrecognized distance model: %s.\n' % model)
