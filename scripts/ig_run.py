124803aee8ac296ee0e7d10957b1f29d1fbbeb32
Federico Tomasi <fdtomasi@gmail.com>
2016-07-01 17:56:37 +0200

    add default parameters

--- scripts/ig_run.py
+++ scripts/ig_run.py
@@ -27,7 +27,8 @@ def main(config_file):
                                        'dialect': 'excel-tab',
                                        'exp_tag': 'debug',
                                        'output_root_folder': 'results',
-                                       'force_silhouette': False})
+                                       'force_silhouette': False,
+                                       'sim_func_args': {}})
 
     db_iter = list(io.read_db(config.db_file, filt=config.apply_filter,
                               dialect=config.dialect))
@@ -48,7 +49,8 @@ def main(config_file):
     root_logger.addHandler(ch)
 
     outfolder, clone_dict = define_clones(db_iter, exp_tag=filename, root=root,
-                                          force_silhouette=config.force_silhouette)
+                                          force_silhouette=config.force_silhouette,
+                                          sim_func_args=config.sim_func_args)
 
     # Copy the ade_config just used into the outFolder
     shutil.copy(config_path, os.path.join(outfolder, 'config.py'))
