798bfad7c69d55036aba61be326dad2815c8089a
Federico T <fdtomasi@gmail.com>
2016-12-13 13:53:14 +0100

    mod parallel distance

--- icing/core/parallel_distance.py
+++ icing/core/parallel_distance.py
@@ -296,7 +296,7 @@ def sm_sparse(X, metric, tol):
     from functools import partial
     job = partial(_job, X=X, tol=tol)
     iterator = list(x for x in pool.imap_unordered(
-        job, combinations(xrange(len(X)), 2), 256) if x is not None)
+        job, combinations(xrange(len(X)), 2), 1048576) if x is not None)
     # iterator = list(
     #     (i, j) for i, j in combinations(xrange(X.shape[0]), 2) if
     #     len(X[i].setV & X[j].setV) > 0 and
