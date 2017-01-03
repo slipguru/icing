8d4367db90d911415e0fe83ce7d9a4aae31b4ccc
Federico Tomasi <fdtomasi@gmail.com>
2017-01-03 16:12:49 +0100

    padding only on columns

--- icing/validation/scores.py
+++ icing/validation/scores.py
@@ -1,5 +1,22 @@
 """Validation utils for clustering algorithms.
 
+Notes
+-----
+Precision, recall and F score
+    In multiclass classification / clustering, a confusion matrix can be
+    obtained. To validate the result, one can use precision, recall and
+    f score. These are obtained using TP, FP, FN, TN.
+    In particular, for each class (true label) x, in a confusion matrix cm:
+    - true positive: diagonal position, cm(x, x).
+    - false positive: sum of column x (without main diagonal),
+        sum(cm(:, x)) - cm(x, x).
+    - false negative: sum of row x (without main diagonal),
+        sum(cm(x, :), 2) - cm(x, x).
+    - true negative: sum of all the matrix without tp, fp, fn.
+
+    Averaging over all classes (with or without weighting) gives values for the
+    entire model.
+
 Author: Federico Tomasi
 Copyright (c) 2016, Federico Tomasi.
 Licensed under the FreeBSD license (see LICENSE.txt).
@@ -11,28 +28,14 @@ import seaborn as sns
 
 
 def get_clones_real_estimated(filename):
-    # res = '/home/fede/projects_local/icing_learning_step_simulated/results/'
-    # 'icing_clones_20.tab_2016-12-12_18.08.55,509543/db_file_clusters.tab'
-    df = pd.read_csv(filename, dialect='excel-tab', header=0)
-
+    """Get true and estimated labels from a partis-generated dataset."""
+    df = pd.read_csv(filename, dialect='excel-tab', header=0,
+                     usecols=('SEQUENCE_ID', 'CLONE'))
     df['CLONE_ID'] = df['SEQUENCE_ID'].str.split('_').apply(lambda x: x[3])
 
-    # confusion matrix
     clone_ids = np.array(df['CLONE_ID'], dtype=str)
     found_ids = np.array(df['CLONE'], dtype=str)
     return clone_ids, found_ids
-
-
-# from sklearn import preprocessing
-# le = preprocessing.LabelEncoder()
-# le.fit(clone_ids)
-# print(le.classes_)
-# print(le.transform(clone_ids))
-
-# let = preprocessing.LabelEncoder()
-# let.fit(found_ids)
-# print(let.classes_)
-# print(let.transform(found_ids))
 
 
 def order_cm(cm):
@@ -77,17 +80,17 @@ def order_cm(cm):
     return b[idx_rows2, :], idx_rows2[idx], idx_cols
 
 
-def confusion_matrix(true_labels, estimated_labels, squared=True,
-                     ordered=True):
+def confusion_matrix(true_labels, estimated_labels, ordered=True):
     """Return a confusion matrix in a multiclass / multilabel problem."""
+    true_labels = np.array(true_labels, dtype=str)
+    estimated_labels = np.array(estimated_labels, dtype=str)
+    if true_labels.shape[0] != estimated_labels.shape[0]:
+        raise ValueError("Inputs must have the same dimensions.")
     rows = np.unique(true_labels)
     cols = np.unique(estimated_labels)
-    if squared:
-        dim = max(rows.shape[0], cols.shape[0])
-        dims = (dim, dim)
-    else:
-        dims = (rows.shape[0], cols.shape[0])
-    cm = np.zeros(dims)
+
+    # padding only on columns
+    cm = np.zeros((rows.shape[0], max(cols.shape[0], rows.shape[0])))
     from collections import Counter
     for i, row in enumerate(rows):
         idx_rows = true_labels == row
@@ -95,12 +98,8 @@ def confusion_matrix(true_labels, estimated_labels, squared=True,
         for g in counter:
             idx_col = np.where(cols == g)[0][0]
             cm[i, idx_col] += counter[g]
-    if squared:
-        mins = min(rows.shape[0], cols.shape[0])
-        add_rows = cols.shape[0] - mins
-        add_cols = rows.shape[0] - mins
-        rows = np.append(rows, ['pad'] * add_rows)
-        cols = np.append(cols, ['pad'] * add_cols)
+
+    cols = np.append(cols, ['pad'] * (cm.shape[1] - cols.shape[0]))
     if ordered:
         cm, rr, cc = order_cm(cm)
         rows, cols = rows[rr], cols[cc]
@@ -139,45 +138,10 @@ def precision_recall_fscore(a, method='micro', beta=1.):
     return precision, recall, fscore
 
 
-
 def show_heatmap(filename):
+    """Show confusion matrix given of a partis-generated tab-delimited db."""
     true_labels, estimated_labels = get_clones_real_estimated(filename)
     cm, rows, cols = confusion_matrix(true_labels, estimated_labels)
     df = pd.DataFrame(cm, index=rows, columns=cols)
     sns.heatmap(df)
     sns.plt.show()
-
-
-def make_square(a, rows=None, cols=None):
-    if a.shape[0] == a.shape[1]:
-        return a
-    mins = min(a.shape)
-    diff_rows = a.shape[1] - mins
-    diff_cols = a.shape[0] - mins
-    if rows is not None and diff_rows > 0:
-        rows = np.append(rows, ['pad'] * diff_rows)
-    if cols is not None and diff_cols > 0:
-        cols = np.append(cols, ['pad'] * diff_cols)
-    return np.pad(a, ((0, diff_rows), (0, diff_cols)),
-                  mode='constant', constant_values=0), rows, cols
-
-
-# For class x:
-# True positive: diagonal position, cm(x, x).
-# False positive: sum of column x (without main diagonal), sum(cm(:, x))-cm(x, x).
-# False negative: sum of row x (without main diagonal), sum(cm(x, :), 2)-cm(x, x).
-# You can compute precision, recall and F1 score following course formula.
-
-# Averaging over all classes (with or without weighting) gives values for the entire model.
-
-def test_validation():
-    a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [2, 5, 7]])
-    a, _, _ = make_square(a)
-    print("Unordered random confusion matrix", a)
-    a, _, _ = order_cm(a)
-    print("Ordered random confusion matrix", a)
-    print(multiclass_TP_FP_FN(a))
-
-
-if __name__ == '__main__':
-    test_validation()
